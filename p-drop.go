98349bb4bbb410a7be25a381ce7e993d3a1368dc
cenkbilgen <cenk.bilgen@gmail.com>
2020-08-05 12:11:46 -0400

    cleanup, comments and unused

--- p-drop.go
+++ p-drop.go
@@ -14,6 +14,8 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"math/rand"
+
 	"strings"
 
 	"github.com/skip2/go-qrcode"
@@ -25,64 +27,48 @@ import (
 
 	"os/signal" // zeroconf captures and handles os signals
 	"syscall"
-	//	"time"
 )
 
 // MARK: HTTP Routes
 
 ////////////////////////////////////////////////////////////////////
 // MARK: Upload Handler
-// response to a recieve request from client
+  // response to a recieve request from  client
 
 func UploadBinaryHandler(response http.ResponseWriter, request *http.Request, _ httprouter.Params) {
 
-	// log.Printf("upload request %v\n", request)
+	log.Printf("upload request %v\n", request)
 
 	// -- start
-	err := request.ParseMultipartForm(100000) //use 1 MB in memory, the rest in disk
+	err := request.ParseMultipartForm(5>>20) //use 5 MB in memory, the rest in disk
 	if err != nil {
 		log.Printf("parse error, %v", err)
 		return
 	}
-
+	// // debug
+	// dump, _ := httputil.DumpRequest(request, true)
+	// ioutil.WriteFile("form_data", dump, 0644)
+	//
 	multipartForm := request.MultipartForm
-	//log.Printf("Form %v\n", multipartForm)
-
-	fileParts := multipartForm.File // File map[string][]*FileHeader
-
-	//valueParts := multipartForm.Value // Value map[string][]string
-	// for n, valuePart := range valueParts {
-	// 	for m, value := range valuePart {
-	// 		log.Printf("value part %v. %v: %v\n", n, m, value)
-	// 	}
-	// }
 
 	// iterate all File Parts of Form
+	fileParts := multipartForm.File
+	log.Printf("Received %v File Parts\n", len(fileParts))
 
-	for n, filePart := range fileParts {
-		//log.Printf("file part %v. has %v file headers\n", n, len(headers))
-		for m, header := range filePart {
+	for key, headers := range fileParts {
+		log.Printf("-- key %v - headers %v\n", key, len(headers))
+		for _, header := range headers {
 			filename := header.Filename
 			size := header.Size
-			//	log.Printf("file part %v. header %v. %v (%v %v)\n", n, m, header, filename, size)
-			log.Printf("receiving %v (%v)\n", filename, size)
-
-			file, err := header.Open() //first check len(headers) is correct  // io.Reader?
+			file, err := header.Open()
 			if err != nil {
-				log.Printf("error part %v %v: %v", n, m, err)
+				log.Printf("Error saving %v (%v)- %v\n", filename, size, err)
 				continue
 			}
-
 			defer file.Close()
-
 			data, err := ioutil.ReadAll(file)
-
 			ioutil.WriteFile(filename, data, 0644)
-
-			// sum := md5.New().Sum(file)
-			// log.Printf("md5: %x\n", sum)
 		}
-
 	}
 
 }
@@ -177,7 +163,7 @@ func AvailableDownloadsHandler(response http.ResponseWriter, request *http.Reque
 		} else {
 			_, filename := filepath.Split(path)
 			size := fileInfo.Size()
-			//log.Printf("%v %v - %v %v\n", fileID, path, filename, size)
+			log.Printf("%v %v - %v %v\n", fileID, path, filename, size)
 
 			//mimetype
 			data, err := ioutil.ReadFile(path)
@@ -211,51 +197,6 @@ func AvailableDownloadsHandler(response http.ResponseWriter, request *http.Reque
 
 }
 
-// // MARK: Device Update ====================================
-// Use for dealing with remote notifications when files available for receiving
-
-// type DeviceUpdateRequest struct {
-// 	DeviceID       string `json:"deviceID"`
-// 	NotificationID string `json:"notificationID"`
-// 	Name           string `json:"name"`
-// }
-//
-// //
-// func DeviceHandler(response http.ResponseWriter, request *http.Request, _ httprouter.Params) {
-//
-// 	var update DeviceUpdateRequest
-//
-// 	decoder := json.NewDecoder(request.Body)
-// 	err := decoder.Decode(&update)
-// 	check_error(err, false)
-//
-// 	filePath := configPath + "/" + update.DeviceID
-//
-// 	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-// 	if err != nil {
-// 		log.Printf("error opening/creating config file for %v. %v", filePath, err)
-// 		response.WriteHeader(http.StatusInternalServerError)
-// 		return
-// 	}
-//
-// 	defer file.Close()
-//
-// 	_, err = file.WriteString(update.Name + "\n" + update.NotificationID)
-// 	if err != nil {
-// 		log.Printf("error writing config file for %v. %v", filePath, err)
-// 		response.WriteHeader(http.StatusInternalServerError)
-// 		return
-// 	}
-//
-// 	response.WriteHeader(http.StatusCreated)
-//
-// }
-
-// ------- Main()
-
-// var notify_chan = make(chan *apns2.Notification, 300)
-// var resp_chan = make(chan *apns2.Response, 300)
-
 var configPath string
 
 func main() {
@@ -263,18 +204,14 @@ func main() {
 	filemap = make(map[string]string)
 
 	// -- Flags
-	// upload := flag.Bool("send", false, "files to send to device")
-	// download := flag.Bool("receive", false, "wait to receive files from device")
 	showQR := flag.Bool("qr", false, "when sending, use QR code to connect devices (if automatic discovery is not working)")
 	port := flag.Int("port", 3000, "listening network port")
-	//setup := flag.Bool("setup", false, "setup new iOS device for notifications")
 
 	flag.Parse()
 
 	var server *zeroconf.Server
 
 	if len(flag.Args()) > 0 { // files specified for uploading
-
 		// remaining arguments should be file paths to upload
 		for n, path := range flag.Args() {
 
@@ -286,14 +223,11 @@ func main() {
 				key := strconv.Itoa(n) // for now just key is the index
 				filemap[key] = path
 			}
-
 		}
-
 	}
 
 	availableDownloads = make([]AvailableDownload, len(filemap))
 
-	//url := "app-p-drop://download?host=" + host + "&port=" + strconv.Itoa(*port) + "&files=1"
 	iface, ip4, err := localIP4()
 	check_error(err, true)
 	host := ip4.String()
@@ -314,7 +248,7 @@ func main() {
 
 	// Start Bonjour/ZeroConf Service Registration
 
-	name := "p-drop"
+	name := "transfer-" + randomString(5)
 	service := "_p-drop._tcp"
 	domain := "local."
 	txt := []string{"txtver=1", "host=" + host, "port=" + portString, "path=" + strings.Join(fileKeys, fileKeysSeparator)}
@@ -322,7 +256,6 @@ func main() {
 	localInterfaces := []net.Interface{*iface}
 	log.Printf("localInterfaces: %v\n", localInterfaces)
 	server, err = zeroconf.Register(name, service, domain, *port, txt, localInterfaces)
-	//server, err = zeroconf.Register(name, service, domain, 3000, []string{"txtver=1", "p=12345"}, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -352,10 +285,6 @@ func main() {
 	router.POST("/upload_binary", UploadBinaryHandler)
 	router.GET("/download_binary", DownloadBinaryHandler)
 	router.GET("/available_downloads", AvailableDownloadsHandler)
-	// router.POST("/device", DeviceHandler) // to exchange
-
-	// set up directories if not setup
-	// TODO: Linux specific, use path packages
 
 	// Configuration
 
@@ -371,63 +300,25 @@ func main() {
 
 	priv_key := configPath + "/server.key"
 	pub_key := configPath + "/server.crt"
-
 	if _, err := os.Stat(priv_key); os.IsNotExist(err) {
 		fmt.Println("Creating TLS key pair.")
 		generate_cert(configPath, "server")
 	}
 
-	//go func() {
-	err = http.ListenAndServeTLS(":"+strconv.Itoa(*port), pub_key, priv_key, router)
-	check_error(err, true)
-	//}()
-
-	// fmt.Printf("Waiting for signals\n")
-	//  s := <-signal_chan
-	//  fmt.Printf("Received signal: %v\n", s)
+	go func() {
+		err = http.ListenAndServeTLS(":"+strconv.Itoa(*port), pub_key, priv_key, router)
+		check_error(err, true)
+	}()
 
 	// MARK: Signal Handler
 	signalChannel := make(chan os.Signal, 1)
-	signal.Notify(signalChannel, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
-	//go handleSignal(signalChannel, server)
-
-	log.Printf("Listening for singals.")
-	// blocks
-	s := <-signalChannel
-
-	log.Printf("Got signal %v. Shutting down\n", s)
-	server.Shutdown()
-
-	// select {
-	// case s:= <-signalChannel:
-	// 	log.Printf("recevied %v\n", s)
-	// default:
-	// 	log.Printf("received nothing yet")
-	// }
-
-}
-
-// MARK: Signal Handler
-
-// NOTE: Blocking
-// NOTE: NOT USED ///////
-func handleSignal(signalChannel <-chan os.Signal, server *zeroconf.Server) {
-	fmt.Printf("Waiting for signals\n")
+	signal.Notify(signalChannel, os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
 
 	select {
-	case s := <-signalChannel:
-		fmt.Printf("Received signal: %v\n", s)
-		switch s {
-		case syscall.SIGTERM, syscall.SIGHUP:
-			fmt.Printf("Shutting down.\n")
-			server.Shutdown()
-			os.Exit(0) // defer is not called on Exit
-		default:
-			fmt.Printf("Not Handling.\n")
-		}
-	default:
-		fmt.Printf("No signal received.\n")
+		case <-signalChannel:
 	}
+
+	log.Printf("Shutting down")
 
 }
 
@@ -450,4 +341,15 @@ func check_error_message(err error, fatal bool, message string) bool {
 		return true
 	}
 	return false
+}
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func randomString(n int) string {
+	alphabetN := len(alphabet)
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = alphabet[rand.Intn(alphabetN)]
+	}
+	return string(b)
 }
